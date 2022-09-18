34587a0165705c6a1e1b7a099853ff40406d95de
YuEgor <rezervdesu15@gmail.com>
2022-09-18 20:18:02 +0800

    Добавил логику

--- ProgTechLab2.3/Form1.cs
+++ ProgTechLab2.3/Form1.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace ProgTechLab2._3
 {
     public partial class Form1 : Form
@@ -5,6 +7,31 @@ namespace ProgTechLab2._3
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Logic
+    {
+        public static string Sort(string Sentense)
+        {
+            string[] wordsArray = Sentense.Split(' ', ',', '.');
+            int[] wordsLenghtArray = new int[wordsArray.Length];
+            for (int i = 0; i < wordsLenghtArray.Length; i++)
+            {
+                wordsLenghtArray[i] = wordsArray[i].Length;
+            }
+
+            Array.Sort(wordsLenghtArray, wordsArray);
+            Array.Reverse(wordsArray);
+
+            string finalLine = String.Join(' ', wordsArray);
+
+            return finalLine;
         }
     }
 }
